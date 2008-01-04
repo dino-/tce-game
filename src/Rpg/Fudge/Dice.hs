0e4322cd4f401dee65c4ca4883982342fa47b7e0
Dino Morelli <dino@ui3.info>
2008-01-04 04:40:19 +0000

    Added a comment for fudgeDieRange

--- src/Rpg/Fudge/Dice.hs
+++ src/Rpg/Fudge/Dice.hs
@@ -7,6 +7,9 @@ import Rpg.Fudge.Trait
 import System.Random (StdGen)
 
 
+{- Fudge dice are 6-sided dice with two + sides, two - sides, and two 
+   blank sides. The possible values of each die are [-1, 0, 1]
+-}
 fudgeDieRange :: (Int, Int)
 fudgeDieRange = (-1, 1)
 
