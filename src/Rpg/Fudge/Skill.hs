8ea4817c1e9c30a3d00db6a9bc8cd40a59abc3cb
Dino Morelli <dino@ui3.info>
2008-01-07 03:08:04 +0000

    Added real skillAdvDiff function
    Added the 'outer' function (:: Skill -> Int) that will call our original one (:: Level -> Level -> Int)

--- src/Rpg/Fudge/Skill.hs
+++ src/Rpg/Fudge/Skill.hs
@@ -24,8 +24,11 @@ veryHard = Level   2
 {- Calculate the multipler for assessing an advancement in a specific 
    skill. This depends on the train difficulty of the skill.
 -}
-skillAdvDiff :: Level -> Level -> Int
-skillAdvDiff (Level difficulty) (Level trait)
+skillAdvDiff :: Skill -> Int
+skillAdvDiff s = skillAdvDiff' (trainLevel s) (skillLevel s)
+
+skillAdvDiff' :: Level -> Level -> Int
+skillAdvDiff' (Level difficulty) (Level trait)
    | adjusted < 0     = 2 ^ 0  -- All of these eval to 1
    | otherwise = 2 ^ (adjusted + 1)
    where adjusted = trait + difficulty
