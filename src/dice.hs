be6931631b8bcd2c8178b3e96888e79a4babc9ee
Dino Morelli <dino@ui3.info>
2010-12-15 22:40:05 +0000

    Added usage integrated with error reporting
    Also using better binding variable names in parseDiceCmd.

--- src/dice.hs
+++ src/dice.hs
@@ -10,19 +10,63 @@ import Rpg.Fudge.Dice ( rolldF )
 import Rpg.Fudge.Trait ( Level (..), ldispL )
 
 
+usage :: String -> String
+usage msg = init . unlines $
+   [ msg
+   , ""
+   , "Usage: dice [<NUMDICE>]d<TYPE> [<NUMROLLS>]"
+   , "Generate random dice rolls."
+   , ""
+   , "NUMDICE defaults to 1 if not specified"
+   , ""
+   , "Types:"
+   , "  20 - 20 sided die"
+   , "  12 - 12 sided die"
+   , "  10 - 10 sided die"
+   , "   8 -  8 sided die"
+   , "   6 -  6 sided die"
+   , "   4 -  4 sided die"
+   , "   % - percentile (value between 1 and 100)"
+   , "   F - Fudge die (value of -1, 0 or 1)"
+   , ""
+   , "Output:  SUM  [INDIVIDUAL DIE VALUES]"
+   , "  Note that Fudge output is also annoted with the standard Fudge scale:"
+   , "     4 Legendary (Superb + 1)"
+   , "     3 Superb"
+   , "     2 Great"
+   , "     1 Good"
+   , "     0 Fair"
+   , "    -1 Mediocre"
+   , "    -2 Poor"
+   , "    -3 Terrible"
+   , "    -4 Terrible - 1"
+   , ""
+   , "Examples:"
+   , "  dice d20  =>  11  [11]"
+   , "  dice 3d6  =>  13  [2,5,6]"
+   , "  dice 2d%  =>  114  [44,70]"
+   , "  dice 4dF  =>  +1 Good  [0,-1,1,1]"
+   ]
+
+
 parseArgList :: [String] -> (String, Int)
-parseArgList []                = error "NO ARGUMENTS"
+parseArgList []                = error . usage $ "NO ARGUMENTS"
 parseArgList (diceCmd:[])      = (map toLower diceCmd, 1)
 parseArgList (diceCmd:rolls:_) = (map toLower diceCmd, read rolls)
 
 
 parseDiceCmd :: String -> (Int, String)
-parseDiceCmd = (first pf) . ((takeWhile p) &&& (tail . (dropWhile p)))
-   where
-      p = (/= 'd')
+parseDiceCmd = (first parseNumDice) .
+   ((takeWhile pr) &&& (tailF . (dropWhile pr)))
 
-      pf "" = 1
-      pf s  = read s
+   where
+      pr = (/= 'd')
+
+      parseNumDice "" = 1
+      parseNumDice s  = read s
+
+      tailF [] = error . usage $ "NO DIE TYPE"
+      tailF t  = tail t
 
 
 rollFunction :: String -> (Int -> StdGen -> [Int])
@@ -34,7 +78,7 @@ rollFunction "10" = roll (1, 10)
 rollFunction "8"  = roll (1, 8)
 rollFunction "6"  = roll (1, 6)
 rollFunction "4"  = roll (1, 4)
-rollFunction _    = error "NO DIE TYPE"
+rollFunction _    = error . usage $ "UNKNOWN DIE TYPE"
 
 
 display :: String -> [Int] -> IO ()
