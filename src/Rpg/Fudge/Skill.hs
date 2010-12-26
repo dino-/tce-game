aeb65a990c856db1da5b5e1ac5268e35927ae477
Dino Morelli <dino@ui3.info>
2010-12-26 20:56:22 +0000

    Simplified Trait code, specifically the Level type
    Since Level is really just an Int, decided to change this to a type synonym and worry about making a distinction at display time. This means a lot of typeclass instance code was removed from this module. And also the display functions needed to be changed. Also, added a new Trait type to combine a trait description string and a Level.

--- src/Rpg/Fudge/Skill.hs
+++ src/Rpg/Fudge/Skill.hs
@@ -14,11 +14,11 @@ data Skill = Skill
 
 
 veryEasy, easy, average, hard, veryHard :: Level
-veryEasy = Level (-2)
-easy     = Level (-1)
-average  = Level   0
-hard     = Level   1
-veryHard = Level   2
+veryEasy = (-2)
+easy     = (-1)
+average  =   0
+hard     =   1
+veryHard =   2
 
 
 {- Calculate the multipler for assessing an advancement in a specific 
@@ -28,7 +28,7 @@ skillAdvDiff :: Skill -> Int
 skillAdvDiff s = skillAdvDiff' (trainLevel s) (skillLevel s)
 
 skillAdvDiff' :: Level -> Level -> Int
-skillAdvDiff' (Level difficulty) (Level trait)
+skillAdvDiff' difficulty trait
    | adjusted < 0     = 2 ^ 0  -- All of these eval to 1
    | otherwise = 2 ^ (adjusted + 1)
    where adjusted = trait + difficulty
--- src/Rpg/Fudge/Trait.hs
+++ src/Rpg/Fudge/Trait.hs
@@ -4,74 +4,63 @@ module Rpg.Fudge.Trait
 import Text.Printf
 
 
-data Level = Level Int
+type Level = Int
+
+data Trait = Trait String Level
    deriving Show
-
-instance Eq Level where
-   (Level l1) == (Level l2) = l1 == l2
-
-instance Num Level where
-   (Level l1) + (Level l2) = Level (l1 + l2)
-   (Level l1) * (Level l2) = Level (l1 * l2)
-   abs (Level l) = Level $ abs l
-   signum (Level l) = Level $ signum l
-   fromInteger n = Level $ fromInteger n
-
-instance Ord Level where
-   (Level l1) <= (Level l2) = l1 <= l2
 
 
 {- Construct the shortest possible description of these trait values
    short of using numbers alone
 -}
 ldisp2 :: Level -> String
-ldisp2 (Level l)
+ldisp2 l
    | l >   3  = 'S' : show (l - 3)
    | l < (-3) = 'T' : show (abs (l + 3))
-ldisp2 (Level   3 ) = "Su"
-ldisp2 (Level   2 ) = "Gr"
-ldisp2 (Level   1 ) = "Go"
-ldisp2 (Level   0 ) = "Fa"
-ldisp2 (Level (-1)) = "Me"
-ldisp2 (Level (-2)) = "Po"
-ldisp2 (Level (-3)) = "Te"
-ldisp2 _            = undefined
+ldisp2   3  = "Su"
+ldisp2   2  = "Gr"
+ldisp2   1  = "Go"
+ldisp2   0  = "Fa"
+ldisp2 (-1) = "Me"
+ldisp2 (-2) = "Po"
+ldisp2 (-3) = "Te"
+ldisp2 _    = undefined
 
 
 {- Construct a short description of these trait values that includes 
    the numbers
 -}
 ldisp4 :: Level -> String
-ldisp4 l@(Level m) = printf "%+d%s" m (ldisp2 l)
+ldisp4 l = printf "%+d%s" l (ldisp2 l)
 
 
 {- Construct a long, human-readable description of a trait value
 -}
 ldisp :: Level -> String
-ldisp (Level   4 ) = "Legendary (Superb + 1)"
-ldisp (Level   3 ) = "Superb"
-ldisp (Level   2 ) = "Great"
-ldisp (Level   1 ) = "Good"
-ldisp (Level   0 ) = "Fair"
-ldisp (Level (-1)) = "Mediocre"
-ldisp (Level (-2)) = "Poor"
-ldisp (Level (-3)) = "Terrible"
-ldisp (Level (-4)) = "Terrible - 1"
-ldisp (Level   l ) = "Level " ++ show l
+ldisp   4  = "Superb + 1"
+ldisp   3  = "Superb"
+ldisp   2  = "Great"
+ldisp   1  = "Good"
+ldisp   0  = "Fair"
+ldisp (-1) = "Mediocre"
+ldisp (-2) = "Poor"
+ldisp (-3) = "Terrible"
+ldisp (-4) = "Terrible - 1"
+ldisp   l  = "Level " ++ show l
 
 
 {- Construct a long, human-readable description of a trait value
    that includes the full number
 -}
 ldispL :: Level -> String
-ldispL l@(Level m) = printf "%+d %s" m (ldisp l)
+ldispL l = printf "%+d %s" l (ldisp l)
 
 
 superb, great, good, fair, mediocre, poor, terrible :: Level
-superb   = Level   3
-great    = Level   2
-good     = Level   1
-fair     = Level   0
-mediocre = Level (-1)
-poor     = Level (-2)
-terrible = Level (-3)
+superb   =   3
+great    =   2
+good     =   1
+fair     =   0
+mediocre = (-1)
+poor     = (-2)
+terrible = (-3)
