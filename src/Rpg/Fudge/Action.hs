462ad237f725df3bee25769e7702b42153c0e7a8
Dino Morelli <dino@ui3.info>
2010-12-26 21:07:41 +0000

    Greatly simplified Action code
    Reduced this to just simple rolls against Fudge levels, instead of expecting high-level types for characters, skills, etc.

--- src/Rpg/Fudge/Action.hs
+++ src/Rpg/Fudge/Action.hs
@@ -1,34 +1,26 @@
 module Rpg.Fudge.Action
    where
 
-import qualified Data.Map as Map
 import System.Random
 
-import Rpg.Fudge.Character
 import Rpg.Fudge.Dice
-import Rpg.Fudge.Skill
 import Rpg.Fudge.Trait
 
 
-data Outcome = Outcome
-   { succeeded :: Bool
-   , degree :: Level
-   }
-   deriving Show
+resolveUnopAct :: RandomGen g => Level -> Level -> g -> (Bool, Level)
+resolveUnopAct skill difficulty g =
+   ((rolledDegree >= difficulty), rolledDegree - difficulty)
 
-
-data Action = Action
-   { desc :: String
-   , relatedSkill :: String
-   , difficulty :: Level
-   }
-   deriving Show
-
-
-resolveUnopAct :: Action -> Skill -> Character -> StdGen -> Outcome
-resolveUnopAct act defSkill char g = Outcome (deg >= actDiff) deg
    where
-      actDiff = difficulty act
-      skill = Map.findWithDefault defSkill
-         (skillName defSkill) (skills char)
-      deg = (skillLevel skill) + (rolldFSum 4 g)
+      roll = head . (rolldFSums 4) $ g
+      rolledDegree = skill + roll
+
+
+resolveOppAct :: RandomGen g => Level -> Level -> g -> (Bool, Level)
+resolveOppAct offensiveFactor defensiveFactor g =
+   ((offensiveRoll >= defensiveRoll), offensiveRoll - defensiveRoll)
+
+   where
+      (or:dr:_) = (take 2) . (rolldFSums 4) $ g
+      offensiveRoll = offensiveFactor + or
+      defensiveRoll = defensiveFactor + dr
